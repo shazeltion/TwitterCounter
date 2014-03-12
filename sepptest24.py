036ea0b674fd30f487c53a53fd1d277c1754aef4
root <root@ip-172-31-26-59.us-west-2.compute.internal>
2014-03-12 20:21:42 +0000

    This is the final output before i send to sepp

--- sepptest24.py
+++ sepptest24.py
@@ -11,7 +11,6 @@ import MySQLdb
 from datetime import datetime
 import sys
 
-print "localhost"
 
 # Go to http://dev.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
@@ -56,37 +55,40 @@ class PortfolioListener(StreamListener):
         pattern = re.compile(r'(\$.+?)(?:\s+?|\"|$)')
         tweetTics = re.findall(pattern, tweetText.lower())
         return tweetTics
-    def on_data(self,data):
-        self.newRec = True # Use this Flag to tell the DB process that theres a new record
-        tweetTickers = self.getTweetTics(data)
-        for key in self.portDict.keys():
-            if key in tweetTickers:
-                self.portDict[key] +=1
-        if self.firstTweet ==True:
-                print 'Starting DB Insert Process. Records will be processed every ' + str(self.dbtimer) +' seconds\n\n'
-                target=self.processToDb()
-                self.firstTweet = False #Change flag to false so multiple threads aren't started
-        target = self.processToDb()
-        print "Full Stream: " + data + "Tickers in tweet: "+ str(tweetTickers) + "\nCounts: " + str(self.portDict) +"\n\n"
     def InsertItems(self):
         now = datetime.now()
         now2 = now.strftime("%Y-%d-%m %H:%M:%S")
         z = [(k,v,now2) for k,v in self.portDict.iteritems()]
         return z
     def processToDb(self):
-        print "" + str(self.firstTweet) + 'I am baller'
-        x = DBConnection()
-        if self.firstTweet == False:
-            if self.newRec == True:
-                x.CreateDBTables()
-                itms = self.InsertItems()
-                print itms
-                x.cur.executemany("""INSERT INTO ciqSocial (GVKEY,Count,Datadate) VALUES (%s,%s,%s)""",itms) ##v
-                x.dbcnxn.commit()
-                print 'db commited'
-                #print ("insert into tweetCount(TwitTic,TweetCount) values(?,?,?)",itms)
-                self.newRec = False
-        Timer(self.dbtimer, self.processToDb).start()
+            #print "" + str(self.firstTweet) + 'I am baller'
+            Timer(self.dbtimer, self.processToDb).start()
+            x = DBConnection()
+            if self.firstTweet == False:
+                if self.newRec == True:
+                    #x.CreateDBTables()
+                    itms = self.InsertItems()
+                    #print itms
+                    x.cur.executemany("""INSERT INTO ciqSocial (GVKEY,Count,Datadate) VALUES (%s,%s,%s)""",itms) ##v
+                    x.dbcnxn.commit()
+                    print 'db commited'
+                    #print ("insert into tweetCount(TwitTic,TweetCount) values(?,?,?)",itms)
+                    #self.newRec = False
+                    x.dbcnxn.close()
+            #print "time"
+            #time.sleep(self.dbtimer)
+    def on_data(self,data):
+        self.newRec = True # Use this Flag to tell the DB process that theres a new record
+        tweetTickers = self.getTweetTics(data)
+        for key in self.portDict.keys():
+            if key in tweetTickers:
+                self.portDict[key] +=1
+        if self.firstTweet ==True: 
+                print 'Starting DB Insert Process. Records will be processed every ' + str(self.dbtimer) +' seconds\n\n'
+                target=self.processToDb()
+                self.firstTweet = False #Change flag to false so multiple threads aren't started
+        #target = self.processToDb()
+        #print "Full Stream: " + data + "Tickers in tweet: "+ str(tweetTickers) + "\nCounts: " + str(self.portDict) +"\n\n"
     def listen(self):
         searchPort = self.createSearchString()
         print 'Listening for '+ searchPort + "\n\n"
@@ -95,7 +97,7 @@ class PortfolioListener(StreamListener):
 class DBConnection():
     def __init__(self):
         self.server = "127.0.0.1"
-        print "" + self.server
+        #print "" + self.server
         self.dbname = "TwitterCount"
         self.un = "root"
         self.pw = "root"
@@ -107,9 +109,9 @@ class DBConnection():
         self.cur =self.dbcnxn.cursor()
     def CreateDBTables(self):
         self.cur.execute("create table if not exists TwitterCount.ciqSocial (GVKEY varchar(10) character set utf8,Count int(11),Datadate datetime, Primary Key (GVKEY,Datadate));")
-        print "done"
+        #print "done"
         self.dbcnxn.commit()
 if __name__ == '__main__':
     portfolio = ['msft','ibm','t','amzn','mhf','cvx','xom','goog','jpm','ge','appl','bwa','tsla','intc','sne']
-    port = PortfolioListener(portfolio, auth, 5) #Create instance for portfolio
+    port = PortfolioListener(portfolio, auth, 600) #Create instance for portfolio
     port.listen()
